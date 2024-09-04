926247287a629b622e4198eea3d807de11d74ae6
Olga Karzhova <olgakorzhova@Olgas-Air.lan>
2024-09-04 09:04:54 -0500

    address the comment

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -3,7 +3,9 @@ import "./styling.css";
 function Banner() {
     return (
         <>
-            <h1>Orbit Report</h1>
+            <header>
+                <h1>Orbit Report</h1>
+            </header>
             <p>Click on the buttons to see the satellites in that orbit type</p>
         </>
     );
